b40d30ee61d570bfad49c838ef4cbef5a297d1ba
Vanda10 <vandabest123@gmail.com>
2024-01-05 16:41:12 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <script setup>
-import LoginForm from './components/LoginForm.vue'
+import sidebar from './components/sidebar.vue';
+import LoginForm from './views/LoginForm.vue';
+import ForgetPass from './views/ForgetPass.vue';
+import ResetPass from './views/ResetPass.vue';
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router'
+
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://mkslcxldoihuzcxijabw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rc2xjeGxkb2lodXpjeGlqYWJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzNjE1MTUsImV4cCI6MjAxOTkzNzUxNX0.40TikJDPUS8rgKYv4-YAMG1kvkpv7AzX4AynpRd3d08')
+const router = useRouter()
+
+onMounted(async () => {
+  supabase.auth.getSession().then(({ data }) => {
+    if(data.session) {
+      router.push({ path: '/', replace: true })
+    }else {
+      router.push({ path: '/login', replace: true })
+    }
+  })
+
+  supabase.auth.onAuthStateChange((event, _session) => {
+    if (event == "SIGNED_OUT"){
+      router.push({ path: '/login', replace: true })
+    }else if(event == "SIGNED_IN") {
+      router.push({ path: '/', replace: true })
+    }
+  })
+})
 </script>
 
 <template>
-  <LoginForm />
+  <div>
+    <router-view />
+  </div>
 </template>
-
 <style scoped>
-
 </style>
---
+++ src/components/deletemodal.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-if="isOpen" class="modal-overlay">
+      <div class="modal-container">
+        <div class="modal-header">
+          <h3>Confirmation</h3>
+          <button @click="closeModal">Close</button>
+        </div>
+        <div class="modal-body">
+          <p>Are you sure you want to delete this teacher?</p>
+        </div>
+        <div class="modal-footer">
+          <button @click="confirmDelete">Yes, Delete</button>
+          <button @click="closeModal">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, defineProps, defineEmits } from 'vue';
+  
+  const props = defineProps(['isOpen']);
+  const emits = defineEmits(['close', 'confirm']);
+  
+  const closeModal = () => {
+    emits.close();
+  };
+  
+  const confirmDelete = () => {
+    emits.confirm();
+    closeModal();
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
